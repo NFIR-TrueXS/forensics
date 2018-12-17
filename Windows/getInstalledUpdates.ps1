e0db8eaeff59a0eb77d797f3494eecc574194450
MrPapercut <947626+mrpapercut@users.noreply.github.com>
2018-12-17 12:04:19 +0100

    Add check if there are no updates

--- Windows/getInstalledUpdates.ps1
+++ Windows/getInstalledUpdates.ps1
@@ -20,17 +20,21 @@ Add-Content -Path "$filePath" -Value "OS Build:         $osBuild"
 Add-Content -Path "$filePath" -Value "OS Version:       $osVersion ($osArchitecture)"
 Add-Content -Path "$filePath" -Value ""
 
-foreach($Update in $Searcher.QueryHistory(0, $historyCount)) {
-    $UpdateDate = $Update.Date.ToString("yyyy-MM-dd HH:mm:ss")
-    $UpdateOperation = switch($Update.Operation){
-        1 {"Installation"}; 2 {"Uninstallation"}; 3 {"Other"}
+if ($historyCount -ne 0) {
+    foreach($Update in $Searcher.QueryHistory(0, $historyCount)) {
+        $UpdateDate = $Update.Date.ToString("yyyy-MM-dd HH:mm:ss")
+        $UpdateOperation = switch($Update.Operation){
+            1 {"Installation"}; 2 {"Uninstallation"}; 3 {"Other"}
+        }
+
+        $OutputObj = New-Object -TypeName PSobject
+        $OutputObj | Add-Member -MemberType NoteProperty -Name UpdateTitle -Value $Update.Title
+        $OutputObj | Add-Member -MemberType NoteProperty -Name Date -Value $UpdateDate
+        $OutputObj | Add-Member -MemberType NoteProperty -Name Operation -Value $UpdateOperation
+
+        Add-Content -Path "$filePath" -Value ($OutputObj | Format-List | Out-String).Trim()
+        Add-Content -Path "$filePath" -Value ""
     }
-
-    $OutputObj = New-Object -TypeName PSobject
-    $OutputObj | Add-Member -MemberType NoteProperty -Name UpdateTitle -Value $Update.Title
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Date -Value $UpdateDate
-    $OutputObj | Add-Member -MemberType NoteProperty -Name Operation -Value $UpdateOperation
-
-    Add-Content -Path "$filePath" -Value ($OutputObj | Format-List | Out-String).Trim()
-    Add-Content -Path "$filePath" -Value ""
+} else {
+    Write "No updates found"
 }
